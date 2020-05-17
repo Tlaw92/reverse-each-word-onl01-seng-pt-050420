6493707089bba9fbbfb3053f4ebe2ee47d982db0
travis Lawson <travislawclimb@gmail.com>
2020-05-17 21:01:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
-def reverse_each_word(sentance)
-  sentance.split.collect {|word| word.reverse}.join("")
+# def reverse_each_word(sentance)
+#   sentance.split.collect {|word| word.reverse}.join("")
+# end
+
+
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
-
-
-
-
